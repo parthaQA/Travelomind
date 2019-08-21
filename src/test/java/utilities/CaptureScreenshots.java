c03040ce7b0181471f876006af3d2ee6b1a08afd
Partha Sen <XPO8584@Y9IND.LOCAL>
2019-08-21 22:47:44 +0530

    screenshot file

---
+++ src/test/java/utilities/CaptureScreenshots.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import dd_core.testCore;
+import listener.CustomListener;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CaptureScreenshots extends testCore  {
+
+    ITestResult result;
+
+    public  void screenShot(ITestResult result){
+
+        this.result=result;
+
+
+        File scrShotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(scrShotFile, new File("C:\\Users\\XPO8584\\IdeaProjects\\Travelomind\\Screenshots\\"+result.getMethod().getMethodName()+".jpeg"));
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
